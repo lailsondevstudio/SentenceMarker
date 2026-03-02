6fa630749532aaef3967d8452a9e7746b38621c7
Lailson Developer <lailsoncristiano.ph@gmail.com>
2026-03-01 20:47:48 -0800

    SentenceMarker
    this is sentenceMarker made by Js , of part of freecodecamp series...!

---
+++ SentenceMarker.js
@@ -0,0 +1,23 @@
+let adjective = "brave";
+let noun = "dragon";
+let verb = "angry";
+let place = "mountain";
+let adjective2 = "fiery";
+let noun2 = "apple";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat "
++ noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First Story: " + firstStory);
+
+adjective = "tiny";
+noun = "wizard";
+verb = "excited";
+place = "castle";
+adjective2 = "sparkly";
+noun2 = "cookies";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to ear " + noun2 + ". The " + noun + " lived in a" + place +
+" and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second Story: " + secondStory);
